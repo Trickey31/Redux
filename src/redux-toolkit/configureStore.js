74a3dd39e206cb64252e436d3fba2576012a4416
Trickey <vngthnh999@gmail.com>
2022-08-08 22:26:53 +0700

    Compltetd

--- src/redux-toolkit/configureStore.js
+++ src/redux-toolkit/configureStore.js
@@ -1,11 +1,34 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import counterSlice from "./counterSlice";
+import counterSlice, { incrementByValue } from "./counterSlice";
 import globalSlice from "./globalSlice";
 const reducer = combineReducers({
   counter: counterSlice,
   global: globalSlice,
 });
 
-const store = configureStore({ reducer });
+const loggerMiddleWare = (store) => (next) => (action) => {
+  console.log(action);
+  next(action);
+};
+
+// function loggerMiddleWare(store) {
+//   return function (next) {
+//     return function (action) {};
+//       your code here
+//   };
+// }
+
+const store = configureStore({
+  reducer,
+  middleware: (gDM) => gDM().concat(loggerMiddleWare),
+});
+
+store.subscribe(() => {
+  //javascript observer pattern
+  console.log(`currentState: ${store.getState().counter.count}`);
+});
+store.dispatch(incrementByValue(1));
+store.dispatch(incrementByValue(3));
+store.dispatch(incrementByValue(5));
 
 export default store;
